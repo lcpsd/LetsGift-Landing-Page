a80fb2fb6a8467b6a9fbde5c343cb9d92852a5cc
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2023-01-03 16:07:47 -0300

    fix: BgImage

--- src/components/BgImage.tsx
+++ src/components/BgImage.tsx
@@ -10,7 +10,6 @@ export function BgImage({ children, url }: BgImageProps) {
     return (
         <Flex
             bgImage={url}
-            bgSize={{ base: "auto", lg: "contain" }}
             w="100vw"
             h="100vh"
             justify="center"
