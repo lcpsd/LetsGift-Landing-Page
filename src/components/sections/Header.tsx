13d8944462b1ca288911a7ffa5f23cb476a856a0
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2023-01-04 12:50:07 -0300

    animation: Header items

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Flex, Img, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import { MotionButton, MotionFlex, MotionText } from "../../utils/chakraFramer";
 import { Menu } from "../Menu";
 import { Section } from "../Section";
 
@@ -20,7 +21,10 @@ export function HeaderSection() {
                     direction="column"
                     gap={5}
                 >
-                    <Flex
+                    {/* Title */}
+                    <MotionFlex
+                        initial={{ y: -50, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 1 }}
                         fontSize={{ base: "2.5rem", lg: "4rem", xl: "5rem" }}
                         fontWeight="bold"
                         w="100%"
@@ -31,11 +35,21 @@ export function HeaderSection() {
                         <Flex textAlign="center" justify={{ base: "center", lg: "initial" }}>
                             que <Box color="tertiary" ml={5}>importam!</Box>
                         </Flex>
-                    </Flex>
-                    <Text fontSize={{ base: "1rem", lg: "1.5rem", xl: "2rem" }} textAlign={{ base: "center", lg: "start" }}>
+                    </MotionFlex>
+
+                    {/* Subtitle */}
+                    <MotionText
+                        fontSize={{ base: "1rem", lg: "1.5rem", xl: "2rem" }}
+                        textAlign={{ base: "center", lg: "start" }}
+                        initial={{ y: -50, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 1 }}
+                        transition={{ delay: 0.2 }}
+                    >
                         Nunca mais receba presentes ruins! Crie suas listas de presentes desejados e compartilhe com amigos e familiares.
-                    </Text>
-                    <Button
+                    </MotionText>
+
+                    {/* Button */}
+                    <MotionButton
                         bg="tertiary"
                         colorScheme="purple"
                         fontWeight="semibold"
@@ -43,17 +57,29 @@ export function HeaderSection() {
                         rounded="full"
                         w="200px"
                         fontSize="2xl"
+                        initial={{ y: 50, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 1 }}
+                        transition={{ delay: 0.3 }}
                     >
                         Entrar
-                    </Button>
+                    </MotionButton>
+
                 </Flex>
 
-                <Flex flex="1" align="center" justify="center">
+                {/* Vector */}
+                <MotionFlex
+                    flex="1"
+                    align="center"
+                    justify="center"
+                    initial={{ x: 50, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    transition={{ delay: 0.4 }}
+                >
                     <Img
                         src="/images/vector02.svg"
                         h="100%"
                     />
-                </Flex>
+                </MotionFlex>
 
             </Flex>
         </Section >
--- src/utils/chakraFramer.ts
+++ src/utils/chakraFramer.ts
@@ -1,6 +1,8 @@
-import { Box, BoxProps, Flex, FlexProps, Img, ImgProps } from "@chakra-ui/react"
+import { Box, BoxProps, Button, ButtonProps, Flex, FlexProps, Img, ImgProps, Text, TextProps } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 
 export const MotionImage = motion<Omit<ImgProps, "transition">>(Img)
 export const MotionFlex = motion<Omit<FlexProps, "transition">>(Flex)
 export const MotionBox = motion<Omit<BoxProps, "transition">>(Box)
+export const MotionText = motion<Omit<TextProps, "transition">>(Text)
+export const MotionButton = motion<Omit<ButtonProps, "transition">>(Button)
