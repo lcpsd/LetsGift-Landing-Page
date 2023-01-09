86557f7a9d87e55ad9db6c168ef8edc4f40bce49
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-09 13:12:15 -0300

    opengraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import { BgImage } from "../components/BgImage";
 import { Container } from "../components/Container";
 import { Footer } from "../components/Footer";
@@ -12,6 +13,41 @@ export default function Home() {
 
   return (
     <>
+      <NextSeo
+        title="Lets Gift - Presentes que importam!"
+        description="Nunca mais receba presentes ruins! Crie suas listas de presentes desejados e compartilhe com amigos e familiares."
+        canonical="https://www.letsgift.online"
+        openGraph={{
+          url: 'https://www.letsgift.online',
+          title: 'Lets Gift - Presentes que importam!',
+          description: 'Nunca mais receba presentes ruins! Crie suas listas de presentes desejados e compartilhe com amigos e familiares.',
+          images: [
+            {
+              url: 'https://i.imgur.com/my6xtOy.png',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+            {
+              url: 'https://i.imgur.com/bteF8nf.png',
+              width: 900,
+              height: 800,
+              alt: 'Og Image Alt Second',
+              type: 'image/jpeg',
+            },
+            { url: 'https://i.imgur.com/my6xtOy.png' },
+            { url: 'https://i.imgur.com/my6xtOy.png' },
+          ],
+          site_name: 'LetsGift',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
+
       <Container overflowX="hidden">
         <Indicator />
 
