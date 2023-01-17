31e1df9a61e981a73133ed3dcd357f490623fbb8
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-17 15:17:23 -0300

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,6 +16,7 @@ export function Footer() {
             <Text textAlign="center">
                 Copyright Letsgift {currentYear}. Todos os direitos reservados.
             </Text>
+
         </Flex>
     )
 }
